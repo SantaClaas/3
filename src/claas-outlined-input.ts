97eb172e5b08eaf84b9e490bbf62c877fd93cb54
Claas <claas.moehlmann@outlook.com>
2023-03-17 03:20:26 +0100

    Scaffold project using open web components

---
+++ src/claas-outlined-input.ts
@@ -0,0 +1,3 @@
+import { ClaasOutlinedInput } from './ClaasOutlinedInput.js';
+
+window.customElements.define('claas-outlined-input', ClaasOutlinedInput);
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { ClaasOutlinedInput } from './ClaasOutlinedInput.js';
