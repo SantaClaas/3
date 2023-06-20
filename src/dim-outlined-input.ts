6ced09eb6a9d320757d288b4bd779c746a0ec6f3
Claas <claas.moehlmann@outlook.com>
2023-06-20 17:34:35 +0200

    Clean up styles

--- src/dim-outlined-input.ts
+++ src/dim-outlined-input.ts
@@ -44,8 +44,8 @@ export class DimOutlinedInput extends LitElement {
       /* border-color: var(--md-sys-color-outline); */
     }
 
-    /* normalize */
-    input {
+    /* normalize
+        input {
       font-family: inherit;
       font-size: 100%;
       font-weight: inherit;
@@ -53,80 +53,69 @@ export class DimOutlinedInput extends LitElement {
       color: inherit;
       margin: 0;
       padding: 0;
-      /* Using invisible border so label text and content text are on same height */
+       Using invisible border so label text and content text are on same height
       border-width: 1px 0;
       border-color: transparent;
     }
+    */
 
     .group {
       position: relative;
       border-radius: 0.375rem;
-    }
 
-    .group input {
-      transition-property: all;
-      transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-      transition-duration: 150ms;
-      background-color: var(--md-sys-color-surface);
+      & input {
+        all: unset;
+        box-sizing: border-box;
+        transition-property: all;
+        transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+        transition-duration: 150ms;
+        background-color: var(--md-sys-color-surface);
 
-      /* outline-none */
-      outline: 2px solid transparent;
-      outline-offset: 2px;
+        /* outline-none */
+        outline: 2px solid transparent;
+        outline-offset: 2px;
 
-      /* leading-none */
-      line-height: 1;
+        /* leading-none */
+        line-height: 1;
 
-      padding: 0.75rem 1rem;
+        padding: 14px 1rem;
 
-      border-radius: 0.375rem;
+        border-radius: 0.375rem;
 
-      touch-action: manipulation;
+        touch-action: manipulation;
 
-      width: 100%;
+        width: 100%;
 
-      /* Styling text (and caret) */
-      caret-color: var(--md-sys-color-primary);
-      color: var(--md-sys-color-on-surface);
-      font-family: var(--md-sys-typescale-body-large-font-family-name);
-      line-height: var(--md-sys-typescale-body-large-line-height);
-      font-size: var(--md-sys-typescale-body-large-font-size);
-      font-weight: var(--md-sys-typescale-body-large-font-weight);
-      letter-spacing: var(--md-sys-typescale-body-large-letter-spacing);
-    }
+        /* Styling text (and caret) */
+        caret-color: var(--md-sys-color-primary);
+        color: var(--md-sys-color-on-surface);
+        font-family: var(--md-sys-typescale-body-large-font-family-name);
+        line-height: var(--md-sys-typescale-body-large-line-height);
+        font-size: var(--md-sys-typescale-body-large-font-size);
+        font-weight: var(--md-sys-typescale-body-large-font-weight);
+        letter-spacing: var(--md-sys-typescale-body-large-letter-spacing);
+        border-width: 1px 0;
+      }
 
-    .group .fillers {
-      /* touch-manipulation */
-      touch-action: manipulation;
-
-      /* origin-top-left */
-      transform-origin: top left;
-
-      /* w-full */
-      width: 100%;
-
-      /* h-full */
-      height: 100%;
-
-      /* flex */
-      display: flex;
-
-      /* absolute */
-      position: absolute;
-      /* top-0 */
-      top: 0;
-
-      /* left-0 */
-      left: 0;
-
-      /* pointer-events-none */
-      pointer-events: none;
-
-      /* Define this to be inherited by filler segments */
-      --border-color: var(--md-sys-color-outline);
-    }
-
-    .group {
       & .fillers {
+        touch-action: manipulation;
+
+        width: 100%;
+
+        height: 100%;
+
+        display: flex;
+
+        position: absolute;
+        top: 0;
+        left: 0;
+
+        /* pointer-events-none */
+        pointer-events: none;
+
+        /* Define this to be inherited by filler segments */
+        --border-color: var(--md-sys-color-outline);
+
         & .filler-start {
           border-width: 1px 0 1px 1px;
           border-radius: 0.375rem 0 0 0.375rem;
@@ -136,13 +125,31 @@ export class DimOutlinedInput extends LitElement {
 
         & .filler-middle {
           padding: 1px 0 0 0;
-          max-width: calc(100% - 24px);
           width: auto;
           border-width: 1px 0;
           border-color: var(--border-color);
 
-          /* ? */
-          flex: 0 0 auto;
+          & label {
+            /* Allow users to copy code and inputs divert focus to their input on click.
+               The other elements have no pointer events because they don't have text and overlap the input that should be selected */
+            pointer-events: auto;
+            display: block;
+            color: var(--md-sys-color-on-surface-variant);
+
+            white-space: nowrap;
+
+            transition-property: all;
+            transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+            transition-duration: 150ms;
+
+            translate: 0 50%;
+
+            font-family: var(--md-sys-typescale-body-large-font-family-name);
+            line-height: var(--md-sys-typescale-body-large-line-height);
+            font-size: var(--md-sys-typescale-body-large-font-size);
+            font-weight: var(--md-sys-typescale-body-large-font-weight);
+            letter-spacing: var(--md-sys-typescale-body-large-letter-spacing);
+          }
         }
 
         & .filler-end {
@@ -154,96 +161,95 @@ export class DimOutlinedInput extends LitElement {
       }
     }
 
-    .group .fillers .filler-middle label {
-      /* Allow users to copy code and inputs divert focus to their input on click.
-      The other elements have no pointer events because they don't have text and overlap the input that should be selected */
-      pointer-events: auto;
-      display: block;
-      color: var(--md-sys-color-on-surface-variant);
-
-      white-space: nowrap;
-
-      transition-property: all;
-      transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-      transition-duration: 150ms;
-
-      translate: 0 0.75rem;
-
-      font-family: var(--md-sys-typescale-body-large-font-family-name);
-      line-height: var(--md-sys-typescale-body-large-line-height);
-      font-size: var(--md-sys-typescale-body-large-font-size);
-      font-weight: var(--md-sys-typescale-body-large-font-weight);
-      letter-spacing: var(--md-sys-typescale-body-large-letter-spacing);
-    }
+    /* States:
+      - focus
+      - empty
+      - hover
+      - disabled
+      - error */
 
     /* FOCUS */
     /* Only remove border top when label moves up e.g focus, not-empty, placeholder shown */
-    .group:focus-within .fillers {
-      --border-color: var(--md-sys-color-primary);
-    }
-    .group:focus-within .fillers .filler-start {
-      border-width: 2px 0 2px 2px;
-    }
-    .group:focus-within .fillers .filler-middle {
-      padding-top: 2px;
-      border-width: 0 0 2px 0;
-      /* border-t-transparent */
-      border-top-color: transparent;
+    .group:focus-within {
+      & input {
+        color: var(--md-sys-color-primary);
+      }
+
+      & .fillers {
+        /* Overwrite border color */
+        --border-color: var(--md-sys-color-primary);
+
+        & .filler-start {
+          border-width: 2px 0 2px 2px;
+        }
+
+        & .filler-middle {
+          /* Padding to account for border size increase. This avoids the label layout shift */
+          padding-top: 2px;
+          border-width: 0 0 2px 0;
+          border-top-color: transparent;
+
+          & label {
+            /* translate: 0 -0.5rem; */
+            translate: 0 -50%;
+            color: var(--md-sys-color-primary);
+            font-size: var(--md-sys-typescale-body-small-font-size);
+            line-height: var(--md-sys-typescale-body-small-line-height);
+          }
+        }
+
+        & .filler-end {
+          border-width: 2px 2px 2px 0;
+        }
+      }
     }
 
-    .group:has(input:not([empty])) .fillers .filler-middle {
-      border-top-color: transparent;
-    }
-    .group:has(input:not([empty])) .fillers .filler-middle label {
-      padding: 0 1px;
-      translate: 0 -0.5rem;
-      font-size: var(--md-sys-typescale-body-small-font-size);
-      line-height: var(--md-sys-typescale-body-small-line-height);
-    }
-
-    .group:focus-within .fillers .filler-end {
-      border-width: 2px 2px 2px 0;
-    }
-
-    /* Only move label up when label moves up e.g focus, not-empty, placeholder shown */
-    /* Yes could be input:focus */
-    .group:focus-within input {
-      color: var(--md-sys-color-primary);
-    }
-
-    /* Only move label up when label moves up e.g focus, not-empty, placeholder shown */
-    .group:focus-within .fillers .filler-middle label {
-      translate: 0 -0.5rem;
-      color: var(--md-sys-color-primary);
-      font-size: var(--md-sys-typescale-body-small-font-size);
-      line-height: var(--md-sys-typescale-body-small-line-height);
+    /* NOT EMPTY */
+    .group:has(input:not([empty])) {
+      & .fillers .filler-middle {
+        border-top-color: transparent;
+        & label {
+          translate: 0 -0.5rem;
+          font-size: var(--md-sys-typescale-body-small-font-size);
+          line-height: var(--md-sys-typescale-body-small-line-height);
+        }
+      }
     }
 
     /* DISABLED */
-    .group:has(input:disabled) .fillers {
-      --border-color: color-mix(
-        in srgb,
-        var(--md-sys-color-on-surface) 12%,
-        transparent
-      );
-    }
-
-    .group:has(input:disabled) .fillers .filler-middle label,
-    input:disabled {
-      color: color-mix(
+    .group:has(input:disabled) {
+      --disable-color: color-mix(
         in srgb,
         var(--md-sys-color-on-surface) 38%,
         transparent
       );
+
+      & input {
+        color: var(--disable-color);
+      }
+
+      & .fillers {
+        --border-color: color-mix(
+          in srgb,
+          var(--md-sys-color-on-surface) 12%,
+          transparent
+        );
+
+        & .filler-middle label {
+          color: var(--disable-color);
+        }
+      }
     }
 
-    /* HOVER */
-    .group:hover:not(:focus-within) .fillers {
-      --border-color: var(--md-sys-color-on-surface);
-    }
+    /* HOVER (Focus should overwrite hover) */
+    .group:hover:not(:focus-within) {
+      & .fillers {
+        --border-color: var(--md-sys-color-on-surface);
 
-    .group:hover:not(:focus-within) .fillers .filler-middle input {
-      color: var(--md-sys-color-on-surface);
+        & .filler-middle input {
+          color: var(--md-sys-color-on-surface);
+        }
+      }
     }
   `;
 }
