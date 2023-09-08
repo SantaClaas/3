0a22ede2e9455e247d63f9039293c7d1fd1bce7d
Claas <claas.moehlmann@outlook.com>
2023-09-08 13:16:53 +0200

    Split top app bar out of layout to avoid future separation of concerns issues

--- components/layout/src/DimLayout.ts
+++ components/layout/src/DimLayout.ts
@@ -2,72 +2,6 @@ import { LitElement, css, html } from 'lit';
 
 export class DimLayout extends LitElement {
   static styles = css`
-    @keyframes fill-color-and-elevate {
-      to {
-        background-color: var(--md-sys-color-surface-container);
-        z-index: var(--md-sys-elevation-level-2-z-index);
-        box-shadow: var(--md-sys-elevation-level-2-shadow);
-      }
-    }
-
-    header {
-      position: sticky;
-      inset: 0 0 auto 0;
-
-      --_bar-height: 64px;
-      width: 100%;
-      height: var(--_bar-height);
-
-      z-index: var(--md-sys-elevation-level-0-z-index);
-
-      box-sizing: border-box;
-      padding: 8px 4px;
-      display: grid;
-      grid-template-columns: auto 1fr minmax(48px, auto);
-      gap: 6px;
-
-      /* TODO on scroll background-color: var(--md-sys-color-surface-container); */
-      background-color: var(--md-sys-color-surface);
-      /* TODO on scroll box-shadow: var(--md-sys-elevation-level-2); */
-      box-shadow: var(--md-sys-elevation-level-0);
-      color: var(--md-sys-color-on-surface);
-
-      animation: fill-color-and-elevate linear both;
-      animation-timeline: scroll();
-      animation-range: 0 var(--_bar-height);
-
-      /* Headline font */
-      font-family: var(--md-sys-typescale-title-large-font-family-name);
-      font-style: var(--md-sys-typescale-title-large-font-family-style);
-      font-weight: var(--md-sys-typescale-title-large-font-weight);
-      font-size: var(--md-sys-typescale-title-large-font-size);
-      letter-spacing: var(--md-sys-typescale-title-large-tracking);
-      line-height: var(--md-sys-typescale-title-large-height);
-      text-transform: var(--md-sys-typescale-title-large-text-transform);
-      text-decoration: var(--md-sys-typescale-title-large-text-decoration);
-
-      & slot:not([name='headline'])::slotted(*) {
-        display: grid;
-        align-content: center;
-        justify-content: center;
-        height: 48px;
-        width: 48px;
-
-        cursor: pointer;
-      }
-
-      & slot[name='headline'] {
-        display: grid;
-        justify-content: center;
-        padding: 0;
-        text-align: center;
-      }
-
-      & slot[name='trailing-icon'] {
-        color: var(--md-sys-color-on-surface-variant);
-      }
-    }
-
     slot:not([name]) {
       display: block;
 
@@ -78,12 +12,23 @@ export class DimLayout extends LitElement {
 
   protected render() {
     return html`
-      <!-- Top App Bar -->
-      <header>
-        <slot name="leading-navigation-icon"></slot>
-        <slot name="headline"></slot>
-        <slot name="trailing-icon"></slot>
-      </header>
+      <dim-top-app-bar>
+        <button slot="leading-navigation-icon">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24"
+            viewBox="0 -960 960 960"
+            width="24"
+          >
+            <path
+              d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"
+              fill="currentColor"
+            />
+          </svg>
+        </button>
+
+        <h1 slot="headline">Dim Components</h1>
+      </dim-top-app-bar>
       <slot></slot>
     `;
   }
--- components/layout/src/dim-layout.ts
+++ components/layout/src/dim-layout.ts
@@ -1,6 +1,8 @@
 import { DimLayout } from './DimLayout.js';
+import { DimTopAppBar } from './DimTopAppBar.js';
 
 /**
  * Order of registratiaon matters! (For components that depend on the lifecycle events of other components)
  */
+window.customElements.define('dim-top-app-bar', DimTopAppBar);
 window.customElements.define('dim-layout', DimLayout);
